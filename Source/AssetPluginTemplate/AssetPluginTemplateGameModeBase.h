65e3023c4831d64480b5c953009961950065833e
Kevin <khdavis4@gmail.com>
2023-05-31 12:30:43 -0400

    Asset Plugin Template Setup
    Initial commit to setup example asset plugin repository

---
+++ Source/AssetPluginTemplate/AssetPluginTemplateGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AssetPluginTemplateGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASSETPLUGINTEMPLATE_API AAssetPluginTemplateGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
